7a7c867f3140b4cb455f6f28b9276e8d3db4c4b3
Fernando <fh1273509@gmail.com>
2022-11-11 21:22:57 -0300

    los nombres ya se filtran con js

---
+++ codigo.js
@@ -0,0 +1,31 @@
+//array
+const nombres = [
+    "MARTIN",
+    "SUSANA",
+    "GISELA",
+    "MARIO",
+    "KARINA",
+    "ERNESTO",
+    "DAIANA",
+    "JUAN",
+    "AGUSTINA",
+    "SERGIO",
+    "LAURA",
+    "ALEJANDRO",
+  ];
+  
+  function listaItems(nombres){
+    let items = "";
+    for(let i = 0; i < nombres.length; i++){
+        items += `<li>${nombres[i]}</li>`;
+    }
+    return items;
+  }
+  document.querySelector("main").innerHTML = `
+  <ol>
+  ${listaItems(nombres)}
+  </ol>
+  `;
+
+const cambios = nombres.filter(value => value.length < 6);
+console.log(cambios);
