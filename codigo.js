de6ccd175ca0d8b9cb2b97137fabf6e1e3e233c7
Fernando <fh1273509@gmail.com>
2022-11-13 21:07:26 -0300

    fin del proyecto

--- codigo.js
+++ codigo.js
@@ -1,37 +1,34 @@
 //array
 const nombres = [
-    "MARTIN",
-    "SUSANA",
-    "GISELA",
-    "MARIO",
-    "KARINA",
-    "ERNESTO",
-    "DAIANA",
-    "JUAN",
-    "AGUSTINA",
-    "SERGIO",
-    "LAURA",
-    "ALEJANDRO",
-  ];
-  
-  function listaItems(nombres){
-    let items = "";
-    for(let i = 0; i < nombres.length; i++){
-        items += `<li>${nombres[i]}</li>`;
-    }
-    return items;
+  "MARTIN",
+  "SUSANA",
+  "GISELA",
+  "MARIO",
+  "KARINA",
+  "ERNESTO",
+  "DAIANA",
+  "JUAN",
+  "AGUSTINA",
+  "SERGIO",
+  "LAURA",
+  "ALEJANDRO",
+];
+
+function listaItems(nombres) {
+  let items = "";
+  for (let i = 0; i < nombres.length; i++) {
+    items += `<li>${nombres[i]}</li>`;
   }
-  document.querySelector("main").innerHTML = `
+  return items;
+}
+document.querySelector("main").innerHTML = `
   <ol>
   ${listaItems(nombres)}
   </ol>
   `;
 
-const cambios = nombres.filter(value => value.length < 6);
+const cambios = nombres.filter((value) => value.length >= 6);
 
-function cambiar(){
+function cambiar() {
   document.write(cambios);
 }
-
-
-
